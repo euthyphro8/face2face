764ae5b2b4aa66307e81a3eea17ccf73d9feaba1
Josh <joshhess13@gmail.com>
2019-02-11 10:51:04 -0700

    Added a contact screen and reworked the string assets a little.

---
+++ app/src/main/java/somethingspecific/face2face/activities/ContactActivity.kt
@@ -0,0 +1,13 @@
+package somethingspecific.face2face.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import somethingspecific.face2face.R
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
--- app/src/main/java/somethingspecific/face2face/activities/LoginActivity.kt
+++ app/src/main/java/somethingspecific/face2face/activities/LoginActivity.kt
@@ -1,7 +1,10 @@
 package somethingspecific.face2face.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_connection.*
+import kotlinx.android.synthetic.main.activity_login.*
 import somethingspecific.face2face.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +12,19 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        login_btn.setOnClickListener {
+            var username = user_text.text?.toString() ?: ""
+            var email = email_text.text?.toString() ?: ""
+            onLogin(username, email)
+        }
+    }
+
+    private fun onLogin(username: String, email: String) {
+        switchToNextView()
+    }
+
+    private fun switchToNextView() {
+        var contactIntent = Intent(this, ContactActivity::class.java)
+        startActivity(contactIntent)
     }
 }
