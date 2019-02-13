42d138b12dd0aea819f29c0c8d7d15f783e06d70
Josh <joshhess13@gmail.com>
2019-02-13 10:35:51 -0700

    Added a video call activity.

--- app/src/main/java/somethingspecific/face2face/activities/ContactActivity.kt
+++ app/src/main/java/somethingspecific/face2face/activities/ContactActivity.kt
@@ -1,7 +1,9 @@
 package somethingspecific.face2face.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_contact.*
 import somethingspecific.face2face.R
 
 class ContactActivity : AppCompatActivity() {
@@ -9,5 +11,18 @@ class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+        call_btn.setOnClickListener {
+            var client = ""
+            onCall(client)
+        }
+    }
+
+    private fun onCall(client: String) {
+        switchToNextView()
+    }
+
+    private fun switchToNextView() {
+        var callIntent = Intent(this, CallActivity::class.java)
+        startActivity(callIntent)
     }
 }
